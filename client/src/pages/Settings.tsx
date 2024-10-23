ecb5e648ded8f43bcc029f8fd25e6ad8e7b2cd4d
Brock Bena <bbena99@gmail.com>
2024-10-23 15:32:06 -0500

    Theme switching is working.

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -8,12 +8,23 @@ interface SettingsPropsI{
 export function Settings(props:SettingsPropsI){
   const {theme,setTheme} = {...props};
   return <>
-    <div>
-      <div className="bg-bg1 text-text">
-        Current Theme: {theme}
-        <Button onClick={()=>setTheme(THEME[(THEME.indexOf(theme)+1)%THEME.length])}>
-          cycle Theme
-        </Button>
+    <div className="p-4">
+      Current Theme: {theme}
+      <div className="flex p-1 bg-bg2 text-text">
+        {THEME.map((constTheme)=>{
+          return (
+            <Button
+              key={constTheme+"_selector"}
+              type="button"
+              onClick={()=>{
+                if(constTheme===theme)return;
+                setTheme(constTheme);
+              }}
+              className= {`w-14 h-14 m-2 rounded-full focus:ring-0 ${theme===constTheme?('border-4 border-bg1'):('')}`}
+              style={{backgroundColor:`var(--color-${constTheme})`}}
+            />
+          )
+        })}
       </div>
     </div>
   </>
