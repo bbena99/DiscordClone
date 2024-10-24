fd335d42a89e7d39a6fd4efbb42945732e62b87c
Brock Bena <bbena99@gmail.com>
2024-10-24 11:07:54 -0500

    Added a search bar

--- client/src/pages/Direct.tsx
+++ client/src/pages/Direct.tsx
@@ -1,11 +1,16 @@
+import { TextInput } from "flowbite-react";
+
 interface DirectPropsI{
   friends:string[];
   directMessages:string[]
 }
 export function Direct(props:DirectPropsI){
   const {friends,directMessages} = {...props}
-  console.log('ping')
   return <>
-    Direct.tsx works
+    <div id='subchannel_selector' className="w-60 h-full bg-bg2">
+      <div id="Search_container" className="w-full h-12 p-2">
+        <TextInput type="TextInput" placeholder="Find or Start a Conversation"/>
+      </div>
+    </div>
   </>
 }
