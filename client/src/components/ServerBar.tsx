ef72f94911ee861dfebc214191c443868640f1eb
Brock Bena <bbena99@gmail.com>
2024-10-24 09:52:03 -0500

    Created the "add server" button

--- client/src/components/ServerBar.tsx
+++ client/src/components/ServerBar.tsx
@@ -1,6 +1,7 @@
-import { FaDiscord } from "react-icons/fa";
+import { FaDiscord, FaPlus } from "react-icons/fa";
 import { ServerI } from "../models";
 import { RiSettings5Fill } from "react-icons/ri";
+import { HR } from "flowbite-react";
 
 interface ServerBarPropsI{
   ServerArray:ServerI[]
@@ -9,10 +10,10 @@ export function ServerBar(props:ServerBarPropsI):JSX.Element{
   const {ServerArray} = {...props}
 
   return (
-    <div className="w-20 h-screen p-1 bg-bg1 flex flex-wrap">
-      <ul style={{height:'calc(100% - 4.5rem)'}}>
+    <div className="w-20 h-screen bg-bg1 flex flex-wrap">
+      <ul className="overflow-scroll" style={{height:'calc(100% - 5rem)'}}>
         <li 
-          className="w-16 h-16 m-1 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
+          className="w-16 h-16 m-2 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
           style={{borderRadius:'50%'}}
           >
           <a className="flex justify-center items-center w-full h-full text-6xl" href="/discord/@me/">
@@ -23,7 +24,7 @@ export function ServerBar(props:ServerBarPropsI):JSX.Element{
           return (
             <li 
             key={server.key}
-            className="w-16 h-16 m-1 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
+            className="w-16 h-16 m-2 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
             style={{borderRadius:'50%'}}
             >
               <a className="flex justify-center items-center w-full h-full text-6xl" href={'/discord/'+server.key+'/'}>
@@ -36,9 +37,18 @@ export function ServerBar(props:ServerBarPropsI):JSX.Element{
             </li>
           )
         })}
+        <HR className="h-0.5 m-3 bg-bg2"/>
+        <li 
+          className="w-16 h-16 m-2 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
+          style={{borderRadius:'50%'}}
+          >
+          <a className="flex justify-center items-center w-full h-full text-3xl" href="/discord/@me/">
+            <FaPlus />
+          </a>
+        </li>
       </ul>
       <div
-        className="w-16 h-16 m-1 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
+        className="w-16 h-16 m-2 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
         style={{borderRadius:'50%'}}
       >
         <a className="flex justify-center items-center w-full h-full text-5xl" href='/discord/settings'>
