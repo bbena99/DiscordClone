b2c3ed3f487d428151df7bf8747d4bbb71df3e92
Brock Bena <bbena99@gmail.com>
2024-10-24 11:26:48 -0500

    Added scrolling without the scrollbar.

--- client/src/components/ServerBar.tsx
+++ client/src/components/ServerBar.tsx
@@ -2,21 +2,23 @@ import { FaDiscord, FaPlus } from "react-icons/fa";
 import { ServerI } from "../models";
 import { RiSettings5Fill } from "react-icons/ri";
 import { HR } from "flowbite-react";
+import { useState } from "react";
+import { NewServerM } from "./NewServerM";
 
 interface ServerBarPropsI{
-  ServerArray:ServerI[]
+  ServerArray:ServerI[];
 }
 export function ServerBar(props:ServerBarPropsI):JSX.Element{
   const {ServerArray} = {...props}
-
+  const [newServerModal,setNewServerModal] = useState<boolean>(false);
   return (
     <div className="w-20 h-screen bg-bg1 flex flex-wrap">
-      <ul className="overflow-scroll" style={{height:'calc(100% - 5rem)'}}>
+      <ul className="overflow-y-auto hiddenscroll" style={{height:'calc(100% - 5rem)'}}>
         <li 
           className="w-16 h-16 m-2 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
           style={{borderRadius:'50%'}}
           >
-          <a className="flex justify-center items-center w-full h-full text-6xl" href="/discord/@me/">
+          <a className="flex justify-center items-center w-full h-full text-5xl" href="/discord/@me/">
             <FaDiscord />
           </a>
         </li>
@@ -27,7 +29,7 @@ export function ServerBar(props:ServerBarPropsI):JSX.Element{
             className="w-16 h-16 m-2 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
             style={{borderRadius:'50%'}}
             >
-              <a className="flex justify-center items-center w-full h-full text-6xl" href={'/discord/'+server.key+'/'}>
+              <a className="flex justify-center items-center w-full h-full text-5xl" href={'/discord/'+server.key+'/'}>
                 {server.src?(
                   <img src={server.src} alt={server.name} />
                 ):(
@@ -42,9 +44,13 @@ export function ServerBar(props:ServerBarPropsI):JSX.Element{
           className="w-16 h-16 m-2 bg-bg2 text-header hover:!rounded-3xl transition-all duration-300 overflow-hidden"
           style={{borderRadius:'50%'}}
           >
-          <a className="flex justify-center items-center w-full h-full text-3xl" href="/discord/@me/">
+          <button
+            type="button"
+            className="flex justify-center items-center w-full h-full text-3xl"
+            onClick={()=>setNewServerModal(true)}
+          >
             <FaPlus />
-          </a>
+          </button>
         </li>
       </ul>
       <div
@@ -55,6 +61,7 @@ export function ServerBar(props:ServerBarPropsI):JSX.Element{
           <RiSettings5Fill/>
         </a>
       </div>
+      <NewServerM newServerModal={newServerModal} setNewServerModal={(b:boolean)=>setNewServerModal(b)}/>
     </div>
   )
 }
