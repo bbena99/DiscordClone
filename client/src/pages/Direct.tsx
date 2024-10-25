2fcd22e15797bc371ac44f8dcdfc388645b0fc49
Brock Bena <bbena99@gmail.com>
2024-10-25 10:45:13 -0500

    Added buttons and channel buttons, no functionality yet.

--- client/src/pages/Direct.tsx
+++ client/src/pages/Direct.tsx
@@ -1,4 +1,6 @@
-import { TextInput } from "flowbite-react";
+import { Button, TextInput } from "flowbite-react";
+import { BsPersonPlusFill } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa";
 
 interface DirectPropsI{
   friends:string[];
@@ -8,8 +10,35 @@ export function Direct(props:DirectPropsI){
   const {friends,directMessages} = {...props}
   return <>
     <div id='subchannel_selector' className="w-60 h-full bg-bg2">
-      <div id="Search_container" className="w-full h-12 p-2">
-        <TextInput type="TextInput" placeholder="Find or Start a Conversation"/>
+      <div id="Search_container" className="w-full h-14 p-3">
+        <TextInput type="TextInput" placeholder="Find or Start a Conversation" className="h-full [&>div]:h-full [&>div>input]:h-full"/>
+      </div>
+      <div className="w-full p-3 [&>button]:w-full [&>button]:bg-bg2 [&>button]:text-header [&>button]:!justify-start">
+        <Button className="[&>span]:py-0.5 [&>span]:text-lg [&>span]:font-semibold [&>span]:flex [&>span]:items-center">
+          <BsPersonPlusFill/>&nbsp;
+          Friends
+        </Button>
+        <Button className="[&>span]:py-0.5 [&>span]:text-lg [&>span]:font-semibold [&>span]:flex [&>span]:items-center">
+          <BsPersonPlusFill/>&nbsp;
+          Nitro
+        </Button>
+        <Button className="[&>span]:py-0.5 [&>span]:text-lg [&>span]:font-semibold [&>span]:flex [&>span]:items-center">
+          <BsPersonPlusFill/>&nbsp;
+          Shop
+        </Button>
+      </div>
+      <div className="p-3">
+        <div className="flex justify-center items-center text-header">
+          <h3 className="text-sm font-semibold" style={{width:'calc(100% - 1rem)'}}>DIRECT MESSAGES</h3>
+          <FaPlus className="w-4 text-sm"/>
+        </div>
+        {directMessages.map((curMessage:string)=>{
+          return(
+            <Button className="w-full bg-bg2 text-text justify-start [&>span]:py-0.5 [&>span]:font-semibold [&>span]:flex [&>span]:items-center">
+              {curMessage}
+            </Button>
+          )
+        })}
       </div>
     </div>
   </>
