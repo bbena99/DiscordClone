e756d7eb18c6047b26f59466828144486be7f1f2
Brock Bena <bbena99@gmail.com>
2024-10-28 14:11:56 -0500

    Added all Friends buttons

--- client/src/components/FriendsC.tsx
+++ client/src/components/FriendsC.tsx
@@ -7,10 +7,28 @@ interface FriendsMPropsI{
 export function FriendsC(props:FriendsMPropsI){
   const {friends} = {...props};
   return (<>
-      <div id="top_bar" className="w-full h-14 bg-bg2">
+      <div id="top_bar" className="w-full h-14 bg-bg2 p-3 flex">
         <Button className="w-36 h-full flex justify-center items-center bg-bg2 !text-header [&>span]:text-lg font-semibold">
           <BsPersonPlus className="text-2xl"/>&nbsp;
           Friends
+        </Button>
+        <Button className="[&>span]:p-1 mx-0.5">
+          Online
+        </Button>
+        <Button className="[&>span]:p-1 mx-0.5">
+          All
+        </Button>
+        <Button className="[&>span]:p-1 mx-0.5">
+          Pending
+        </Button>
+        <Button className="[&>span]:p-1 mx-0.5">
+          Suggestions
+        </Button>
+        <Button className="[&>span]:p-1 mx-0.5">
+          Blocked
+        </Button>
+        <Button className="[&>span]:p-1 mx-0.5">
+          Add Friend
         </Button>
       </div>
       {friends.map((f:string)=>{
