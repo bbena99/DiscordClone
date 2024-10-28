945e8a686f6206699f9463d291054c98a75b2fcb
Brock Bena <bbena99@gmail.com>
2024-10-27 22:50:20 -0500

    Updates

--- client/src/components/FriendsC.tsx
+++ client/src/components/FriendsC.tsx
@@ -1,11 +1,20 @@
+import { Button } from "flowbite-react";
+import { BsPersonPlus } from "react-icons/bs";
+
 interface FriendsMPropsI{
   friends:string[];
 }
 export function FriendsC(props:FriendsMPropsI){
   const {friends} = {...props};
   return (<>
+      <div id="top_bar" className="w-full h-14 bg-bg2">
+        <Button className="w-36 h-full flex justify-center items-center bg-bg2 !text-header [&>span]:text-lg font-semibold">
+          <BsPersonPlus className="text-2xl"/>&nbsp;
+          Friends
+        </Button>
+      </div>
       {friends.map((f:string)=>{
-        return <div>
+        return <div key={f}>
           {f}
         </div>
       })}
