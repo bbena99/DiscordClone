620d14d569ada8b0a84f8666ef46f695b2164f94
Brock Bena <bbena99@gmail.com>
2026-02-21 12:37:55 -0600

    Added Models to the admin window registry

--- server/discord/admin.py
+++ server/discord/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Servers)
+admin.site.register(Folders)
+admin.site.register(Channels)
+admin.site.register(Messages)
+
+admin.site.register(Friends)
+admin.site.register(UsersToServers)
+admin.site.register(ServerToFolders)
+admin.site.register(FolderToChannels)
+admin.site.register(ChannelToMessage)
--- server/discord/views.py
+++ server/discord/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import *
 
 # Create your views here.
 def home(request):
+  user = Users.objects.all()
   return JsonResponse({"name":"Hello World!"})
